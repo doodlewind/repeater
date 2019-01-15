4519f7056984a361c2581c632ae00213f52ebfc6
Yifeng Wang <i@ewind.us>
2019-01-15 22:38:31 +0800

    feat: add clipboard support

--- recorder/browser-action/extension.js
+++ recorder/browser-action/extension.js
@@ -24,7 +24,35 @@ const toCamel = str => str
   .join('')
 
 const updateTabState = () => {
-  console.log('TODO update tab state')
+  const data = {
+    ignoreIdleMove: document.getElementById('ignore-idle-move').checked,
+    throttleDragMove: document.getElementById('throttle-drag-move').checked
+  }
+  requestTab({ type: 'setOptions', data }, () => {})
+}
+
+// https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+const copyToClipboard = (text) => {
+  const textArea = document.createElement('textarea')
+
+  textArea.style.position = 'fixed'
+  textArea.style.top = 0
+  textArea.style.left = 0
+  textArea.style.width = '2em'
+  textArea.style.height = '2em'
+  textArea.style.padding = 0
+  textArea.style.border = 'none'
+  textArea.style.outline = 'none'
+  textArea.style.boxShadow = 'none'
+  textArea.style.background = 'transparent'
+
+  textArea.value = text
+
+  document.body.appendChild(textArea)
+  textArea.focus()
+  textArea.select()
+  document.execCommand('copy')
+  document.body.removeChild(textArea)
 }
 
 const init = () => {
@@ -48,6 +76,12 @@ const init = () => {
       $option.checked = options[toCamel(id)]
     })
   })
+
+  document.getElementById('get-log').onclick = () => {
+    requestTab({ type: 'getLog' }, log => {
+      copyToClipboard(JSON.stringify(log, null, 2))
+    })
+  }
 }
 
 document.addEventListener('DOMContentLoaded', init)
